5bd8efec203343ab45aebc9c00c465f5796f983a
Simon Lambert <s.lambert.pro@gmail.com>
2021-06-24 13:41:28 +0200

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-//Function used on array of objects to sum by selected key
+//Function used on array of objects to sum by selected key returning a new array of objects
 function sumByKey(array,keyName,valueName){
 	return [...new Set(array.map(i => i[keyName]))].map(key => {
 	return {[keyName]: key, sum: array.filter(j => j[keyName] === key).reduce((a,b) => +a + +[b[valueName]],0)}
